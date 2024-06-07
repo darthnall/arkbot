5d08fcc5abdc35d6dfe21ee648120090b85166a2
Blake Nall <blake@proton.me>
2024-06-07 14:02:16 -0500

    Changed default token value to blank str

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import os
 import re
+
 import discord
+
 
 def get_urls(text: str) -> list[str]:
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, text)
     return [_[0] for _ in url]
+
 
 class ArkClient(discord.Client):
     def __init__(self) -> None:
@@ -13,7 +16,7 @@ class ArkClient(discord.Client):
 
     async def on_ready(self) -> None:
         print(f"Logged in as {self.user}.")
-    
+
     async def on_message(self, message) -> None:
         print(f"Message from {message.author}: {message.content}")
         if message.author != self.user:
@@ -21,6 +24,8 @@ class ArkClient(discord.Client):
             if urls:
                 await message.delete()
 
+
 if __name__ == "__main__":
     client = ArkClient()
-    client.run(os.environ.get("DISCORD_TOKEN", None))
+    client.run(os.environ.get("DISCORD_TOKEN", ""))
+
